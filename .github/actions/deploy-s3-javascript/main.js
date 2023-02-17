827f8d64b5dea808ab8e86c0bb6c1b40819a9bb5
Mann <mann.luong@cgu.com.au>
2023-02-17 17:38:56 +1100

    Added output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -13,7 +13,8 @@ function run() {
   const s3Url = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Url} --region ${bucketRegion}`);
 
-  core.notice('Hello from my custom JavaScript Action!');
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput('website-url', websiteUrl);
 }
 
 run();
