00ea97c72a324cfd0c933cc2dc8a9f9c7ec4920e
Mann <mann.luong@cgu.com.au>
2023-02-17 16:39:01 +1100

    added custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,6 +4,15 @@ const exec = require('@actions/exec');
 
 
 function run() {
+  // 1. Get some input values
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // 2. Upload files
+  const s3Url = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Url} --region ${bucketRegion}`);
+
   core.notice('Hello from my custom JavaScript Action!');
 }
 
